0d5fcb7678b135b2004ec56b47ead83985a5613b
tvtest614-eng <tvtest614@gmail.com>
2026-03-01 00:13:05 +0300

    Initial setup for Next Level Supreme
    index.jsx ve package.json dosyaları sisteme yüklendi.

---
+++ next-level-pro/index.jsx
@@ -0,0 +1,11 @@
+{
+"name": "next-level-supreme",
+"version": "1.0.0",
+"private": true,
+"dependencies": {
+"react": "^18.2.0",
+"react-dom": "^18.2.0",
+"firebase": "^10.7.0",
+"lucide-react": "^0.294.0"
+}
+}
